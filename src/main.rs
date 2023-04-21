5671b6b23e533ef66270652307bc8123e7457012
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-04-21 22:06:39 +0530

    File Separator complete

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,50 @@
-use std::{fs, fs::DirEntry, io};
+use std::{fs, fs::DirEntry};
 
-fn files_in_dir() -> io::Result<Vec<DirEntry>> {
-    fs::read_dir(".")?
-        .flatten()
-        .filter(|x| !x.path().is_dir())
-        .map(|x| Ok(x))
-        .collect()
-}
-
-fn toml_filter(files: Vec<DirEntry>) -> Vec<DirEntry> {
-    files
-        .into_iter()
-        .filter(|x| {
-            Some(String::from("toml"))
-                == match x.path().extension() {
-                    Some(val) => val.to_os_string().into_string().ok(),
-                    None => None,
-                }
-        })
-        .collect()
-}
 fn main() {
-    let files = files_in_dir();
-    //println!("{:?}", files);
-    let ok_files = match files {
-        Ok(val) => val,
-        Err(e) => {
-            println!("{:?}", e);
-            Vec::new()
+    let files: Vec<DirEntry> = fs::read_dir(".")
+        .unwrap()
+        .flatten()
+        .filter(|x| x.path().is_file())
+        .collect();
+    let mut document_files: Vec<DirEntry> = Vec::new();
+    let mut media_files: Vec<DirEntry> = Vec::new();
+    let mut pictures_files: Vec<DirEntry> = Vec::new();
+    let document_ext = [
+        Some(String::from("pdf")),
+        Some(String::from("epub")),
+        Some(String::from("docx")),
+    ];
+    let media_ext = [
+        Some(String::from("mp4")),
+        Some(String::from("mkv")),
+        Some(String::from("mp3")),
+        Some(String::from("flac")),
+    ];
+    let pictures_ext = [
+        Some(String::from("jpg")),
+        Some(String::from("png")),
+        Some(String::from("gif")),
+    ];
+    println!("{:?}", files);
+    for file in files {
+        if document_ext.contains(&match file.path().extension() {
+            Some(val) => val.to_os_string().into_string().ok(),
+            None => None,
+        }) {
+            document_files.push(file);
+        } else if media_ext.contains(&match file.path().extension() {
+            Some(val) => val.to_os_string().into_string().ok(),
+            None => None,
+        }) {
+            media_files.push(file);
+        } else if pictures_ext.contains(&match file.path().extension() {
+            Some(val) => val.to_os_string().into_string().ok(),
+            None => None,
+        }) {
+            pictures_files.push(file);
         }
-    };
-    //println!("{:?}", ok_files)
-    let toml_file = toml_filter(ok_files);
-    println!("{:?}", toml_file);
+    }
+    println!("{:?}", document_files);
+    println!("{:?}", media_files);
+    println!("{:?}", pictures_files);
 }
